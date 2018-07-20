7f7f9fc18e3392dce5071712bc7d1236988094cb
Rahul Sinha <rahul.sinha1908@gmail.com>
2018-07-20 11:32:42 +0530

    Added apidoc documentation

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Therapy on Demand",
+  "version": "0.1.0",
+  "description": "Backend APIs",
+  "title": "Therapy on Demand",
+  "url": "http://api.therapyondemand.io",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-07-20T04:52:16.043Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
