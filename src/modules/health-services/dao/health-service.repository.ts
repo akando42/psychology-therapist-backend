65c9f464bfa55c33eef08ae5a358de94a0d38da6
Miguel-Martinez <miguelesalador@gmail>
2018-09-01 15:40:36 -0400

    health service interface

--- src/modules/health-services/dao/health-service.repository.ts
+++ src/modules/health-services/dao/health-service.repository.ts
@@ -8,4 +8,13 @@ export abstract class AbstractHealthServiceRepository extends AbstractRepository
     constructor(dao: any, converter: any) {
         super(dao, converter)
     }
+
+    abstract createService(service: IHealthService): Promise<IHealthService>;
+
+    abstract deleteService(id: number | string): Promise<boolean>;
+
+    abstract updateService(id: number | string, updatedEntity: IHealthService): Promise<IHealthService>
+
+    
+
 }
--- src/modules/health-services/dao/repositories/my-sql-health-service.repository.ts
+++ src/modules/health-services/dao/repositories/my-sql-health-service.repository.ts
@@ -1,10 +1,20 @@
 import { AbstractHealthServiceRepository } from "../health-service.repository";
+import { IHealthService } from "../../../../models/health-service";
 
 
 
 export class MySqlHealthServiceRepository extends AbstractHealthServiceRepository {
     constructor(dao, converter) {
         super(dao, converter);
+    }
+    createService(service: IHealthService): Promise<IHealthService> {
+        throw new Error("Method not implemented.");
+    }
+    deleteService(id: string | number): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    updateService(id: string | number, updatedEntity: IHealthService): Promise<IHealthService> {
+        throw new Error("Method not implemented.");
     }
 }
 
