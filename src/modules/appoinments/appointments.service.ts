10480af2a9054a4edc2c525b6485e3fb2ab2e1bc
Miguel-Martinez <miguelesalador@gmail>
2018-09-01 15:44:40 -0400

    addpointment service interface

--- src/modules/appoinments/appointments.service.ts
+++ src/modules/appoinments/appointments.service.ts
@@ -1,7 +1,28 @@
 import { IAppointment } from "../../models/appointment";
+import { AppointmentStatusEnum } from "../../enums/appoinments.enum";
 
 
 export interface IAppointmentsService {
 
-    createAppoinment(appointment: IAppointment): Promise<IAppointment>
+    /**
+     * Create and appoinment.
+     * @param appointment 
+     */
+    createAppoinment(appointment: IAppointment): Promise<IAppointment>;
+
+    /**
+     * appointments get canceled.
+     * @param appointmentId 
+     */
+    cancelAppointment(appointmentId: IAppointment): Promise<boolean>;
+
+    /**
+     * Change the status of a appointment, its a partial update ONLY to the status
+     * of the appointment
+     * @param appoinmentId 
+     * @param newStatus 
+     */
+    changeAppointmentStatus(appoinmentId: string | number, newStatus: AppointmentStatusEnum): Promise<IAppointment>;
+
+
 }
